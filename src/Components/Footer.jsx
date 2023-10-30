b2836a5211eda0660742799eb40e5c20f152cd9e
Manshi Malik <140046353+manshi8923@users.noreply.github.com>
2023-10-30 16:41:12 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,13 +11,13 @@ const Footer = () => {
       <div>
         <h5>Follow Us</h5>
         <div>
-          <a href="https://youtube.com/6packprogrammer" target={"blank"}>
+          <a href="https://youtube.com/" target={"blank"}>
             Youtube
           </a>
-          <a href="https://instagram.com/meabhisingh" target={"blank"}>
+          <a href="https://instagram.com" target={"blank"}>
             Instagram
           </a>
-          <a href="https://github.com/meabhisingh" target={"blank"}>
+          <a href="https://github.com" target={"blank"}>
             Github
           </a>
         </div>
